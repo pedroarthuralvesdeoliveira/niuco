bd22d84544260db29e09c222a11675f5c55eae4b
Pedro <pedroarthuralvesdeoliveira1@gmail.com>
2024-06-25 20:09:49 -0300

    Fix trailing-whitespace

--- challenge/config.py
+++ challenge/config.py
@@ -1,14 +1,12 @@
-from pydantic import Field
-from pydantic_settings import BaseSettings, SettingsConfigDict
-
+from pydantic import BaseSettings, Field
+from pydantic_settings import SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-    env_file='.env', 
-    env_file_encoding='utf-8'
+        env_file='.env',
+        env_file_encoding='utf-8'
     )
 
     api_url: str = Field(..., env='API_URL')
-
 
 settings = Settings()
