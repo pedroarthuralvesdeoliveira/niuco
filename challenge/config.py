8b95dfd0dd9199e376910a6bbd615dcd7c48227d
Pedro <pedroarthuralvesdeoliveira1@gmail.com>
2024-06-25 20:11:16 -0300

    Lint all checks passed

--- challenge/config.py
+++ challenge/config.py
@@ -1,12 +1,13 @@
 from pydantic import BaseSettings, Field
 from pydantic_settings import SettingsConfigDict
 
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file='.env',
-        env_file_encoding='utf-8'
+        env_file='.env', env_file_encoding='utf-8'
     )
 
     api_url: str = Field(..., env='API_URL')
+
 
 settings = Settings()
