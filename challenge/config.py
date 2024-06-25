16957059d4baba2d5c9f6495fa7b96f5c535689f
Pedro <pedroarthuralvesdeoliveira1@gmail.com>
2024-06-25 20:06:09 -0300

    Fix lint error line too long

--- challenge/config.py
+++ challenge/config.py
@@ -3,7 +3,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+    model_config = SettingsConfigDict(
+        env_file='.env', 
+        env_file_encoding='utf-8'
+    )
 
     api_url: str = Field(..., env='API_URL')
 
