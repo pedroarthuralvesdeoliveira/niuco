dbada39cf8e667fbeb617b5fb77a85ade74f1ec6
Pedro <pedroarthuralvesdeoliveira1@gmail.com>
2024-06-25 20:04:31 -0300

    Fix not finding .env

--- challenge/config.py
+++ challenge/config.py
@@ -1,18 +1,10 @@
-# from pydantic import Field
-# from pydantic_settings import BaseSettings, SettingsConfigDict
-
-
-# class Settings(BaseSettings):
-#
-
-
-# settings = Settings()
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=..., env_file_encoding='utf-8')
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+
     api_url: str = Field(..., env='API_URL')
 
 
