5060175d5048bc94aa39fcf328486ec623a73187
Pedro <pedroarthuralvesdeoliveira1@gmail.com>
2024-06-25 20:12:39 -0300

    Changed basesettings from pydantic to pydantic settings

--- challenge/config.py
+++ challenge/config.py
@@ -1,5 +1,5 @@
-from pydantic import BaseSettings, Field
-from pydantic_settings import SettingsConfigDict
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
